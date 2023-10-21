7473a433dcbfe2d1541e3319c2855b5cd74a8316
Nisfan <mohnisfan@gmail.com>
2023-10-21 22:36:18 +0530

    write message

--- app/SSE.tsx
+++ app/SSE.tsx
@@ -1,27 +1,27 @@
 "use client";
-import { useRouter } from "next/navigation";
 import React from "react";
-import { useTransition } from "react";
 
 export default function SSE() {
-  // React.useEffect(() => {
-  //   const eventSource = new EventSource(`/api/page_sse`, {
-  //     withCredentials: true,
-  //   });
-  //   eventSource.onopen = () => {
-  //     console.log("open");
-  //   };
-  //   eventSource.onmessage = (e) => {
-  //     console.log(e.data);
-  //   };
-  //   eventSource.onerror = (e) => {
-  //     console.log(e);
-  //   };
-  //
-  //   return () => {
-  //     eventSource.close();
-  //   };
-  // }, []);
-  //
+  const eventSource = new EventSource(`/api/sse`);
+
+  React.useEffect(() => {
+    eventSource.addEventListener("some-event", (e) => {
+      console.log(e.data);
+    });
+    // eventSource.onopen = () => {
+    //   console.log("open");
+    // };
+    // eventSource.onmessage = (e) => {
+    //   console.log(e.data);
+    // };
+    // eventSource.onerror = (e) => {
+    //   console.log(e);
+    // };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
   return null;
 }
--- app/api/sse/route.ts
+++ app/api/sse/route.ts
@@ -1,17 +1,42 @@
 // import type { NextApiRequest, NextApiResponse } from "next";
 
 import { stream } from "@/app/serverActions";
+import { NextResponse } from "next/server";
 // export const delay = (ms: number) =>
 //   new Promise<void>((resolve) => setTimeout(resolve, ms));
 
 export const dynamic = "force-dynamic";
 // export const runtime = "edge"; // 'nodejs' (default) | 'edge'
+function writeMessage(
+  writer: WritableStreamDefaultWriter,
+  encoder: TextEncoder,
+  message: any,
+): void {
+  if (message.comment) {
+    void writer.write(encoder.encode(`: ${message.comment}\n`));
+  }
+  if (message.event) {
+    void writer.write(encoder.encode(`event: ${message.event}\n`));
+  }
+  if (message.id) {
+    void writer.write(encoder.encode(`id: ${message.id}\n`));
+  }
+  if (message.retry) {
+    void writer.write(encoder.encode(`retry: ${message.retry}\n`));
+  }
+  // if (message.data) {
+  //   void writer.write(encoder.encode(toDataString(message.data)));
+  // }
+}
 
 export async function GET() {
-  let responseStream = new TransformStream();
-  const writer = responseStream.writable.getWriter();
+  const stream = new TransformStream();
+  const writer = stream.writable.getWriter();
   const encoder = new TextEncoder();
 
+  writeMessage(writer, encoder, {
+    event: "some-event",
+  });
   // const resp = new EventSource("https://next-app-sse.vercel.app/api/sse");
   // resp.onmessage = async (e) => {
   //   await writer.write(encoder.encode(`event: message\ndata: ${e.data}\n\n`));
@@ -22,10 +47,11 @@ export async function GET() {
   //   await writer.close();
   // };
   //
-  return new Response(responseStream.readable, {
+  return new NextResponse(stream.readable, {
     headers: {
       "Content-Type": "text/event-stream",
       "Cache-Control": "no-cache, no-transform",
+      Connection: "keep-alive",
     },
   });
 }
